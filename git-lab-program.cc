88461d565b1d0990697b5e37c493d11e8e51a919
CamRayle <70271493+CamRayle@users.noreply.github.com>
2020-09-04 15:47:18 -0400

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Cameron Rayle>
+ *        Date: <9/4/2020>
+ * Description:git-lab-2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /cout << "Hello Git!!" << endl;/
+    return 0;
+}// main
